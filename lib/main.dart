b9a2afe2ad5d5b7ad04e49f44dd651ca3c5ffc89
kcsanju444 <124895709+kcsanju444@users.noreply.github.com>
2024-01-03 21:57:48 +0545

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:note/model/NoteOperation.dart';
+import 'package:note/model/note.dart';
 import 'package:note/screen/HomeScreen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/home',
-    routes: {
-      '/home': (context) => const HomeScreen(),
-    },
+  runApp(ChangeNotifierProvider<NoteOperation>(
+    create: (context) => NoteOperation(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const HomeScreen(),
+      },
+    ),
   ));
 }
